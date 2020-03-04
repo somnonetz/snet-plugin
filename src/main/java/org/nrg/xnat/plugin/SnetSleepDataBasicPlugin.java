a46215c0dccd8a756bbae7f03ddeeede0032eea5
jmz-b <jaymuhz.bee@gmail.com>
2020-03-04 17:42:09 +0100

    configure remaining data types with class annotations

--- src/main/java/org/nrg/xnat/plugin/SnetSleepDataBasicPlugin.java
+++ src/main/java/org/nrg/xnat/plugin/SnetSleepDataBasicPlugin.java
@@ -5,6 +5,22 @@ import org.nrg.framework.annotations.XnatPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@XnatPlugin(value = "snet-plugin", name = "SNET SleepDataBasic", description = "The SnetSleepDataBasicPlugin contains data types for SleepResearchSessionData, psgScanData, psgEDFData and psgRecordData")
+@XnatPlugin(value = "snet-plugin", name = "SNET SleepDataBasic", description = "The SnetSleepDataBasicPlugin contains data types for SleepResearchSessionData, psgScanData, psgEDFData and psgRecordData",
+        dataModels = {@XnatDataModel(value = "snet01:SleepResearchSession",
+                                       singular = "Sleep Research Session",
+                                       plural = "Sleep Research Session",
+                                       code = "SRS"),
+                      @XnatDataModel(value = "snet01:psgScanData",
+                                       singular = "PSG Recording",
+                                       plural = "PSG Recordings",
+                                       code = "PSG"),
+                      @XnatDataModel(value = "snet01:psgEDFData",
+                                       singular = "EDF Recording",
+                                       plural = "EDF Recordings",
+                                       code = "EDF"),
+                      @XnatDataModel(value = "snet01:psgRecordData",
+                                       singular = "EDF Channel",
+                                       plural = "EDF Channels",
+                                       code = "CHA")})
 public class SnetSleepDataBasicPlugin {
 }
