a7612f59febe304c5a9a0f2948e71da3c8aee3cb
jmz-b <jaymuhz.bee@gmail.com>
2020-03-18 17:40:50 +0100

    remove psgEDFData schema
    this data type is not currently being used as we are not reconstructing
    our EDFs, so remove it

--- src/main/java/org/nrg/xnat/plugin/SnetSleepDataBasicPlugin.java
+++ src/main/java/org/nrg/xnat/plugin/SnetSleepDataBasicPlugin.java
@@ -5,22 +5,18 @@ import org.nrg.framework.annotations.XnatPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@XnatPlugin(value = "snet-plugin", name = "SNET SleepDataBasic", description = "The SnetSleepDataBasicPlugin contains data types for SleepResearchSessionData, psgScanData, psgEDFData and psgRecordData",
+@XnatPlugin(value = "snet-plugin", name = "SNET SleepDataBasic", description = "Contains data types for SleepResearchSessionData, psgScanData, psgRecordData",
         dataModels = {@XnatDataModel(value = "snet01:SleepResearchSession",
-                                       singular = "Sleep Research Session",
-                                       plural = "Sleep Research Session",
-                                       code = "SRS"),
+                                     singular = "Sleep Research Session",
+                                     plural = "Sleep Research Sessions",
+                                     code = "SRS"),
                       @XnatDataModel(value = "snet01:psgScanData",
-                                       singular = "PSG Recording",
-                                       plural = "PSG Recordings",
-                                       code = "PSG"),
-                      @XnatDataModel(value = "snet01:psgEDFData",
-                                       singular = "EDF Recording",
-                                       plural = "EDF Recordings",
-                                       code = "EDF"),
+                                     singular = "PSG Recording",
+                                     plural = "PSG Recordings",
+                                     code = "PSG"),
                       @XnatDataModel(value = "snet01:psgRecordData",
-                                       singular = "EDF Channel",
-                                       plural = "EDF Channels",
-                                       code = "CHA")})
+                                     singular = "EDF Channel",
+                                     plural = "EDF Channels",
+                                     code = "CHA")})
 public class SnetSleepDataBasicPlugin {
 }
